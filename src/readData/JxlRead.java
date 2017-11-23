81192de5b37e4fc719505529f2e4bc1cc20831ef
gongyu <735701336@qq.com>
2017-11-23 18:11:19 +0800

    添加 读取 Excel 表内指定单元格内容模块

--- src/readData/JxlRead.java
+++ src/readData/JxlRead.java
@@ -1,4 +1,37 @@
 package readData;
 
+import jxl.*;
+import jxl.read.biff.BiffException;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 public class JxlRead {
+    public JxlRead() {}
+
+    public void getExcel(String filePath) {
+        try {
+            InputStream is = new FileInputStream(filePath);
+            Workbook wb = Workbook.getWorkbook(is);
+            Sheet sheet = wb.getSheet(0);
+            Cell cell = sheet.getCell(0,0);
+            String str = cell.getContents();
+            if (cell.getType() == CellType.LABEL) {
+                LabelCell labelCell = (LabelCell)cell;
+                str = labelCell.getString();
+            }
+            System.out.println(str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BiffException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        JxlRead jxlRead = new JxlRead();
+        jxlRead.getExcel("C:\\Users\\yu\\Desktop\\word\\文档\\中国统计年鉴 2017(EXCEL+目录)\\CH0102.xls");
+    }
+
 }
